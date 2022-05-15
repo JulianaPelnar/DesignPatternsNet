6dec5fee3363a4b9f79cafdd7ff3eb19c65ca242
Juliana Pelnar <julianapelnar@gmail.com>
2022-05-16 00:02:27 +0200

    FunctionalBuilder

---
+++ DesignPatterns/Builder/FunctionalBuilder.cs
@@ -0,0 +1,76 @@
+﻿using System;
+namespace DesignPatterns.Builder.FunctionalBuilder
+{
+	public class Person
+    {
+		public string Name, Position;
+    }
+
+	public abstract class FunctionalBuilder<TSubject, TSelf>
+		where TSelf : FunctionalBuilder<TSubject, TSelf>
+		where TSubject : new()
+    {
+        private readonly List<Func<Person, Person>> actions
+            = new List<Func<Person, Person>>(); // Fluent
+        public TSelf Do(Action<Person> action)
+            => AddAction(action);
+        public Person Build()
+            => actions.Aggregate(new Person(), (p, f) => f(p));
+        private TSelf AddAction(Action<Person> action)
+        {
+            actions.Add(p =>
+            {
+                action(p);
+                return p;
+            });
+            return (TSelf)this;
+        }
+    }
+
+	public sealed class PersonBuilder
+		: FunctionalBuilder<Person, PersonBuilder>
+    {
+		public PersonBuilder Called(string name)
+			=> Do(p => p.Name = name);
+    }
+	// This is commented to show both ways to do the same, although the
+	// uncommented is preferred
+	//public sealed class PersonBuilder // sealed means you can't inherit from it
+	//   {
+	//	private readonly List<Func<Person, Person>> actions
+	//		= new List<Func<Person, Person>>(); // Fluent
+	//	public PersonBuilder Called(string name)
+	//		=> Do(p => p.Name = name);
+	//	public PersonBuilder Do(Action<Person> action)
+	//		=> AddAction(action);
+	//	public Person Build()
+	//		=> actions.Aggregate(new Person(), (p, f) => f(p));
+	//	private PersonBuilder AddAction(Action<Person> action)
+	//       {
+	//		actions.Add(p =>
+	//		{
+	//			action(p);
+	//			return p;
+	//		});
+	//		return this;
+	//       }
+	//   }
+	public static class PersonBuilderExtensions
+    {
+		public static PersonBuilder WorkAs
+			(this PersonBuilder builder, string position)
+			=> builder.Do(p => p.Position = position);
+    }
+	public class FunctionalBuilder
+	{
+		public FunctionalBuilder()
+		{
+			var person = new PersonBuilder()
+				.Called("Sarah")
+				.WorkAs("Developer")
+				.Build();
+			Console.WriteLine($"Name: {person.Name}, Position: {person.Position}");
+		}
+	}
+}
+
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using DesignPatterns.Builder;
+using DesignPatterns.Builder.FunctionalBuilder;
 using DesignPatterns.SolidPrinciples;
 
 Console.WriteLine("Hello, World!");
@@ -12,4 +13,5 @@ Console.WriteLine("Hello, World!");
 //object b = new Builder();
 //object bf = new FluentBuilder();
 //object bfg = new FluentBuilderInheritanceRecursiveGenerics();
-object sw = new StepwiseBuilder();
+//object sw = new StepwiseBuilder();
+object fb = new FunctionalBuilder();

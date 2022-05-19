6feb4ef29af521df708c7af395592b65df26c55e
Juliana Pelnar <julianapelnar@gmail.com>
2022-05-19 20:22:09 +0200

    AsynchronousFactoryMethod

---
+++ DesignPatterns/Factory/AsynchronousFactoryMethod.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace DesignPatterns.Factory
+{
+	public class Foo
+    {
+		private Foo() // si nobody can use it
+        {
+			//
+        }
+		private async Task<Foo> InitAsync() // only used internally
+		{
+			await Task.Delay(1000);
+			return this;
+		}
+		public static Task<Foo> CreateAsync()
+        {
+			var result = new Foo();
+			return result.InitAsync();
+        }
+    }
+	public class AsynchronousFactoryMethod
+	{
+		public AsynchronousFactoryMethod() { }
+		public async Task<Foo> GetCreateAsync()
+		{
+			var x = await Foo.CreateAsync();
+			return x;
+		}
+	}
+}
+

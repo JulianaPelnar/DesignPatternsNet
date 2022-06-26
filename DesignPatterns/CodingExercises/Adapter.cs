de5385cc904b1eafa91f3b9556d38b6671e452ef
Juliana Pelnar <julianapelnar@gmail.com>
2022-06-26 18:54:20 +0200

    Adapter

---
+++ DesignPatterns/CodingExercises/Adapter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace DesignPatterns.CodingExercises
+{
+    // You're given an IRectangle interface and an extension method on it.
+    // Try to define a SquareToRectangleAdapter that adapts the Square to the
+    // IRectangle interface.
+    public class Square
+    {
+        public int Side;
+    }
+
+    public interface IRectangle
+    {
+        int Width { get; }
+        int Height { get; }
+    }
+
+    public static class ExtensionMethods
+    {
+        public static int Area(this IRectangle rc)
+        {
+            return rc.Width * rc.Height;
+        }
+    }
+
+    public class SquareToRectangleAdapter : IRectangle
+    {
+        private int side;
+        public SquareToRectangleAdapter(Square square)
+        {
+            side = square.Side;
+        }
+
+        public int Width => side;
+
+        public int Height => side;
+    }
+    public class Adapter
+    {
+        public Adapter()
+        {
+        }
+    }
+}
+
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,9 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using DesignPatterns.CodingExercises;
+using DesignPatterns.Adapter.DI;
+using DesignPatterns.Adapter.Caching;
 
 Console.WriteLine("Hello, World!");
 
 //object se = new Singleton();
 //object p = new Prototype();
 //object f = new Factory();
-object b = new Builder();
+//object b = new Builder();
+object a = new AdapterInDependencyInjection();
